24450a632c378bb6fcc96e92f3e32344d66442ea
vandanhtran95 <vandanhtran95@gmail.com>
2025-05-03 01:31:59 +0700

    Update control.js

--- js/control.js
+++ js/control.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 var typed = new Typed('.element', {
-    strings: ["Web Designer.", "Photoshop.","Apps Designer.",],
- typeSpeed:50,
- backSpeed:50,
- loop:true,
- });
+    strings: ["Web Designer.", "Photoshop.", "Apps Designer.",],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
